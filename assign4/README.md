3bbb20dfccd596c27a0c0f13c31b1a3542e9b1ab
Han Tun <45828855+HanTun97@users.noreply.github.com>
2024-03-11 04:39:52 -0500

    assignment4

--- assign4/README.md
+++ assign4/README.md
@@ -16,6 +16,8 @@
 
 ![alt text](https://github.com/HanTunZaw-dev/MDP_AssignAssignment/blob/main/assign4/screenshoots/assign4_8.png?raw=true)
 
+![alt text](https://github.com/HanTunZaw-dev/MDP_AssignAssignment/blob/main/assign4/screenshoots/assign4_8_1.png?raw=true)
+
 ![alt text](https://github.com/HanTunZaw-dev/MDP_AssignAssignment/blob/main/assign4/screenshoots/assign4_9.png?raw=true)
 
 ![alt text](https://github.com/HanTunZaw-dev/MDP_AssignAssignment/blob/main/assign4/screenshoots/assign4_10.png?raw=true)
--- assign4/app/src/main/java/com/example/assign4/ShoppingCategoryActivity.kt
+++ assign4/app/src/main/java/com/example/assign4/ShoppingCategoryActivity.kt
@@ -2,6 +2,7 @@ package com.example.assign4
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.assign4.databinding.ActivityShoppingCategoryBinding
 
 class ShoppingCategoryActivity : AppCompatActivity() {
@@ -13,5 +14,18 @@ class ShoppingCategoryActivity : AppCompatActivity() {
         setContentView(binding.root)
         val userName = intent.getStringExtra("userName")
         binding.idWelcome.text = getString(R.string.welcome, userName)
+
+        binding.home.setOnClickListener{
+            Toast.makeText(this, "${binding.home.contentDescription}", Toast.LENGTH_LONG).show()
+        }
+        binding.beauty.setOnClickListener{
+            Toast.makeText(this, "${binding.beauty.contentDescription}", Toast.LENGTH_LONG).show()
+        }
+        binding.pet.setOnClickListener{
+            Toast.makeText(this, "${binding.pet.contentDescription}", Toast.LENGTH_LONG).show()
+        }
+        binding.toy.setOnClickListener{
+            Toast.makeText(this, "${binding.toy.contentDescription}", Toast.LENGTH_LONG).show()
+        }
     }
 }
